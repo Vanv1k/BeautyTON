c37a2cd8b14173544534967a386e523a674209c6
Ivan Vikulin <ivanspecial5@gmail.com>
2025-07-06 23:12:20 +0300

    feat: 11

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "pong 11")
+		fmt.Fprint(w, "pong 12")
 	})
 
 	fmt.Println("Server running on :8080")
