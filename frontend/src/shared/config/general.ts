c8571affeaf6d37aa1887b3c441d05cf02e1da45
Azat-Bilalov <az.bilalov@mail.ru>
2025-07-09 11:15:49 +0300

    fix: IS_TG_MINIAPP value

--- frontend/src/shared/config/general.ts
+++ frontend/src/shared/config/general.ts
@@ -4,4 +4,8 @@ export const IS_DEV_DOMAIN = DEV_HOSTS.some((host) =>
   window.location.hostname.includes(host),
 );
 
-export const IS_TG_MINIAPP = window.Telegram && window.Telegram.WebApp;
+export const IS_TG_MINIAPP = (() => {
+  const hash = window.location.hash.slice(1);
+  const params = new URLSearchParams(hash);
+  return Boolean(params.get('tgWebAppVersion'));
+})();
