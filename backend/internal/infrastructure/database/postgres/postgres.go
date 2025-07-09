8aacd74b7f643d2a8c35ff20c867666d8dd6e229
Ivan Vikulin <ivanspecial5@gmail.com>
2025-07-09 20:37:53 +0300

    feat: added repository impl

--- backend/internal/infrastructure/database/postgres/postgres.go
+++ backend/internal/infrastructure/database/postgres/postgres.go
@@ -1,9 +1,7 @@
-package posgres
+package postgres
 
 import (
 	"fmt"
-	"time"
-
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 
@@ -24,19 +22,35 @@ func NewPostgresRepo(cfg config.PostgresConfig) (*Postgres, error) {
 		return nil, fmt.Errorf("failed to connect to database: %w", err)
 	}
 
-	// TODO: create config variables for this case
 	sqlDB, err := db.DB()
 	if err != nil {
 		return nil, fmt.Errorf("failed to get sql.DB: %w", err)
 	}
-	sqlDB.SetMaxOpenConns(100)
-	sqlDB.SetMaxIdleConns(10)
-	sqlDB.SetConnMaxLifetime(time.Hour)
+	sqlDB.SetMaxOpenConns(cfg.MaxOpenConns)
+	sqlDB.SetMaxIdleConns(cfg.MaxIdleConns)
+	sqlDB.SetConnMaxLifetime(cfg.ConnMaxLifetime)
 
-	err = db.AutoMigrate(&entity.User{})
+	err = db.AutoMigrate(
+		&entity.User{},
+		&entity.UserPreferences{},
+		&entity.MasterProfile{},
+		&entity.Subscription{},
+		&entity.MyMaster{},
+		&entity.Service{},
+		&entity.ServiceCategory{},
+		&entity.Booking{},
+		&entity.Review{},
+		&entity.Payment{},
+		&entity.City{},
+		&entity.Country{},
+	)
 	if err != nil {
 		return nil, fmt.Errorf("failed to migrate database: %w", err)
 	}
 
 	return &Postgres{db: db}, nil
+}
+
+func (p *Postgres) GetDB() *gorm.DB {
+	return p.db
 }
